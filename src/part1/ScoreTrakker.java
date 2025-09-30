f9633e74a7988836179a3832caf293aebf101574
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-30 15:10:38 -0600

    Stub functions for ScoreTrakker

--- src/part1/ScoreTrakker.java
+++ src/part1/ScoreTrakker.java
@@ -5,4 +5,19 @@ import java.util.ArrayList;
 public class ScoreTrakker {
 	private ArrayList<Student> students = new ArrayList<Student>();
 	
+	public void loadDataFile(String filename) {
+		//stub
+	}
+	
+	public void printInOrder() {
+		//stub
+	}
+	
+	public void processFiles() {
+		//stub
+	}
+	
+	public static void main(String[] args) {
+		//stub
+	}
 }
