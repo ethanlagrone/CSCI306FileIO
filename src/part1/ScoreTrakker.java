e5ded84cdcec0b10cdbeece26f010e948f66d899
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-30 16:07:22 -0600

    Finished part 1

--- src/part1/ScoreTrakker.java
+++ src/part1/ScoreTrakker.java
@@ -1,23 +1,40 @@
 package part1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students = new ArrayList<Student>();
 	
-	public void loadDataFile(String filename) {
-		//stub
+	public void loadDataFile(String filename) throws FileNotFoundException {
+		FileReader reader = new FileReader(filename);
+		Scanner in = new Scanner(reader);
+		while (in.hasNext()) {
+			String name = in.nextLine();     
+			int score = Integer.parseInt(in.nextLine());   
+		    students.add(new Student(name, score));
+		}
+		in.close();
 	}
 	
 	public void printInOrder() {
-		//stub
+		Collections.sort(students);
+		for(Student s : students) {
+			System.out.println(s);
+		}
 	}
 	
-	public void processFiles() {
-		//stub
+	public void processFiles() throws FileNotFoundException {
+		loadDataFile("scores.txt");
+		printInOrder();
 	}
 	
-	public static void main(String[] args) {
-		//stub
+	public static void main(String[] args) throws FileNotFoundException {
+		ScoreTrakker tracker = new ScoreTrakker();
+	    tracker.processFiles();
+		
 	}
 }
